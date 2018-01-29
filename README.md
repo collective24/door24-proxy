7c0c5aba01f4267d68515657041296f32db31eec
Jonatan Dahl <dahl.jonatan@gmail.com>
2018-01-29 01:07:02 +0100

    Whitelist class and async handlers

--- README.md
+++ README.md
@@ -11,5 +11,5 @@ yarn install
 ## Run
 
 ```
-SHEET_ID=<spreadsheet id here> KEY_FILE_PATH='./keyfile.json' yarn start
+SHEET_ID=<spreadsheet id here> KEY_FILE_PATH='./key.json' yarn start
 ```
--- index.js
+++ index.js
@@ -1,35 +1,17 @@
 const express = require('express')
-const GoogleSpreadsheet = require('google-spreadsheet')
+const asyncHandler = require('express-async-handler')
+const { Whitelist } = require('./lib/Whitelist')
 
 const app = express()
-const { SHEET_ID, KEY_FILE_PATH } = process.env
-const WHITELIST_WORKSHEET_NAME = 'whitelist'
-const serviceAccountCredentials = require(KEY_FILE_PATH)
-const doc = new GoogleSpreadsheet(SHEET_ID)
+const whitelist = new Whitelist()
 
-app.get('/whitelist', (req, res) => {
+// TODO: error handling middleware
+
+app.get('/whitelist', asyncHandler(async (req, res, next) => {
+  const list = await whitelist.getAll()
   res.setHeader('Content-Type', 'application/json')
-  doc.useServiceAccountAuth(serviceAccountCredentials, () => {
-    doc.getInfo((err, info) => {
-      if (err) {
-        console.error('Error fething sheet:', err)
-      } else {
-        const sheet = info.worksheets.find(sheet => sheet.title === WHITELIST_WORKSHEET_NAME)
-        sheet.getRows({}, (err, rows) => {
-          if (err) {
-            console.error('Error getting rows:', err)
-          } else {
-            const json = rows.map(row => ({
-              rfid: row.rfid,
-              name: row.name
-            }))
-            res.send(JSON.stringify(json))
-          }
-        })
-      }
-    })
-  })
-})
+  res.json(list)
+}))
 
 app.put('/whitelist', (req, res) => res.send('Hello World!'))
 app.post('/whitelist', (req, res) => res.send('Hello World!'))
