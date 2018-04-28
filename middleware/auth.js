61a04a683e6c64c99b2be87b6ef8c761a5205b51
Jonatan Dahl <dahl.jonatan@gmail.com>
2018-04-28 14:48:07 +0200

    set added date when adding new whitelist entries

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,6 +2,6 @@ module.exports = function (req, res, next) {
   if (req.headers.authorization === process.env.AUTH_TOKEN) {
     next()
   } else {
-    res.send(401)
+    res.sendStatus(401)
   }
 }
